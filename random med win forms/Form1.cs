175a2063a9c152ea2c600aaf6bf33d1a01c822b9
Admin <Admin@TEB127012>
2023-06-02 09:22:19 +0200

    Add project files.

---
+++ random med win forms/Form1.cs
@@ -0,0 +1,25 @@
+namespace random_med_win_forms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
